8dbe89d8b9c4639634d6c7d3d30822b15729dc4f
Samantha Hyatt <shhyatt@gmail.com>
2018-10-31 18:22:41 +0000

    Done.

--- index.js
+++ index.js
@@ -32,13 +32,17 @@ function longPlaneteerCalls(words) {
 }
 
 function findTheCheese (foods) {
-  for(let i=0, i<foods.length, i++) {
-    if(foods[i] === 'cheddar' || foods[i] === 'gouda' || foods[i] === 'camembert') {
+  
+  for(var i=0, i<foods.length, i++) {
+    if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
       return foods[i];
     }
   }
+  return "no cheese!";
+}
+
+  
     
   
     
     
-}
