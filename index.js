25697b06c6fb0bbb99161413ba9bf8dbb5e3aa49
Samantha Hyatt <shhyatt@gmail.com>
2018-10-31 17:10:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function dwarfRollCall(dwarves) {
-}
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"];
 
+function dwarfRollCall(dwarves) {
+  var newArray = []; 
+  for (let i=0; i<dwarves.length;i++){
+    newArray.push(`${i+1}. ${dwarves[i]}` );
+   }
+   return newArray.join("");
+  }
+
+
+
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
 function summonCaptainPlanet(planeteerCalls){
+  var newArray = [];
+  for (var i=0; i<planeteerCalls.length; i++) {
+    newArray.push(planeteerCalls[i].toUpperCase() + '!');
+  }
+  return newArray;
 }
 
 function longPlaneteerCalls(words) {
